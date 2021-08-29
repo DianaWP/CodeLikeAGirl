9cdaa9d38109898c05df16d4f275365b3d40a8a5
DianaWP <DianaWP@users.noreply.github.com>
2021-08-29 13:24:15 +1000

    Add files via upload

---
+++ Lesson2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CodeLikeaGirl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please name a mode of transport");
+            var vehicle = Console.ReadLine();
+            Console.WriteLine("Please pick a destination");
+            var destination = Console.ReadLine();
+            Console.WriteLine("Whats your favourited mythical creature");
+            var creature = Console.ReadLine();
+            
+            vehicle = vehicle.Trim();
+            destination = destination.Trim();
+            creature = creature.Trim();
+
+            Console.WriteLine($"You hopped into your {vehicle} and headed off to {destination} to find the last living {creature}");
+        
+
+        }
+    }
+}
